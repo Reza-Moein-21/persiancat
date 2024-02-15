485fc6819a28e489bc9b43523e620ec95ad214ba
reza_moein <rezamoeinpe@gmail.com>
2024-02-16 03:23:23 +0330

    Make test package modular ,Add Gson to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,18 @@ module com.gmail.rezamoeinpe.persiancat {
     requires ch.qos.logback.core;
     requires ch.qos.logback.classic;
     requires org.slf4j;
+    requires com.google.gson;
 
+    // public exports
     exports com.gmail.rezamoeinpe.persiancat;
     exports com.gmail.rezamoeinpe.persiancat.rest.method;
     exports com.gmail.rezamoeinpe.persiancat.exceptions;
     exports com.gmail.rezamoeinpe.persiancat.exceptions.base;
+
+    // only for test exports
+    exports com.gmail.rezamoeinpe.persiancat.internal.controller to com.gmail.rezamoeinpe.persiancat.test;
+    exports com.gmail.rezamoeinpe.persiancat.internal.http to com.gmail.rezamoeinpe.persiancat.test;
+    exports com.gmail.rezamoeinpe.persiancat.internal.http.parser to com.gmail.rezamoeinpe.persiancat.test;
+    exports com.gmail.rezamoeinpe.persiancat.internal.socket to com.gmail.rezamoeinpe.persiancat.test;
+    exports com.gmail.rezamoeinpe.persiancat.internal.http.handler to com.gmail.rezamoeinpe.persiancat.test;
 }
---
+++ src/test/java/module-info.java
@@ -0,0 +1,9 @@
+open module com.gmail.rezamoeinpe.persiancat.test {
+    requires com.gmail.rezamoeinpe.persiancat;
+    requires org.assertj.core;
+    requires org.junit.jupiter.engine;
+    requires org.mockito.junit.jupiter;
+    requires org.junit.jupiter.api;
+    requires net.bytebuddy;
+    requires net.bytebuddy.agent;
+}
